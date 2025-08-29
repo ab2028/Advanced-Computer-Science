a9b5945b2bd52a943a154c52295fe83e54db2fda
ab2028 <avberenji2024@gmail.com>
2025-08-29 14:59:55 -0700

    Demo u0d2
    Zombies and methods and attributes

---
+++ Unit Lessons/U0D2/Demo.java
@@ -0,0 +1,19 @@
+// Class -> a set of attributes and behaviours
+public class Demo {
+    public static void main(String[] args) {
+        // type main and press enter for a shortcut to ^
+        Demo sample = new Demo(); // sample is called an instance of Demo?
+
+        sample.havingFun();
+
+    }
+
+    public String havingFun() { // Method - OUTSIDE of main
+        return "I'm having fun!";
+
+    }
+
+    public void hello() { // Void methods return nothing, can modify things though
+
+    }
+}
