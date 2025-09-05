8c5e8203acb1f89650c9fcdd838527c1a68466de
ab2028 <avberenji2024@gmail.com>
2025-09-05 09:20:04 -0700

    Rectangle Lab
    did most of it

---
+++ Assignments/Unit 0 Rectangle Lab/Rectangle.java
@@ -0,0 +1,59 @@
+public class Rectangle{
+    // instance variables
+    private int length;
+    private int width;
+
+    // constructors
+    public Rectangle(int newLength, int newWidth){
+        length = newLength;
+        width = newWidth;
+
+    }
+
+    //public Rectangle() { //space or no space after { ?}
+
+    // getters
+    public int getLength(){
+        return length;
+    }
+    public int getWidth(){
+        return width;
+    }
+
+
+    // setters
+    public void setLength(int input){
+        length = input;
+    }
+    public void setWidth(int input){
+        width = input;
+    }
+
+    // methods
+    public String toString(){
+        return "This rectangle has a width of " + width + " and a length of " + length + ".";
+    }
+
+    public boolean equals(Rectangle other){
+        return this.width == other.width && this.length == other.length;
+    }
+
+    public int calculateArea(){
+        return length * width;
+    }
+
+    public int calculatePerimeter(){
+        return 2*(length + width);
+    }
+
+    public double calculateDiagonal(){
+        return Math.sqrt((Math.pow(length,2) + Math.pow(width, 2)));
+    }
+
+
+
+
+
+
+
+}
---
+++ Assignments/Unit 0 Rectangle Lab/RectangleTester.java
@@ -0,0 +1,26 @@
+public class RectangleTester {
+   public static void main(String[] args) {
+    Rectangle rect1 = new Rectangle(1,2);
+    Rectangle rect2 = new Rectangle(3,4);
+
+    System.out.println(rect1.toString());
+    System.out.println(rect2.toString());
+
+
+    rect1.setWidth(5);
+    rect2.setLength(1);
+
+    rect1.getWidth();
+    rect2.getLength();
+    System.out.println(rect1.toString());
+    System.out.println(rect2.toString());
+
+    System.out.println(rect1.equals(rect2));
+
+    System.out.println(rect1.calculateDiagonal());
+    System.out.println(rect2.calculateDiagonal());
+
+
+
+   } 
+}
