7be10e65be1cf5e43bdc96350831a3d6c3953c91
ab2028 <avberenji2024@gmail.com>
2025-09-05 09:42:53 -0700

    Fixed rectangle lab checkstyle
    Fixed rectangle lab checkstyle

--- Assignments/Unit 0 Rectangle Lab/Rectangle.java
+++ Assignments/Unit 0 Rectangle Lab/Rectangle.java
@@ -1,10 +1,10 @@
-public class Rectangle{
+public class Rectangle {
     // instance variables
     private int length;
     private int width;
 
     // constructors
-    public Rectangle(int newLength, int newWidth){
+    public Rectangle(int newLength, int newWidth) {
         length = newLength;
         width = newWidth;
 
@@ -13,41 +13,43 @@ public class Rectangle{
     //public Rectangle() { //space or no space after { ?}
 
     // getters
-    public int getLength(){
+    public int getLength() {
         return length;
     }
-    public int getWidth(){
+
+    public int getWidth() {
         return width;
     }
 
 
     // setters
-    public void setLength(int input){
+    public void setLength(int input) {
         length = input;
     }
-    public void setWidth(int input){
+
+    public void setWidth(int input) {
         width = input;
     }
 
     // methods
-    public String toString(){
+    public String toString() {
         return "This rectangle has a width of " + width + " and a length of " + length + ".";
     }
 
-    public boolean equals(Rectangle other){
+    public boolean equals(Rectangle other) {
         return this.width == other.width && this.length == other.length;
     }
 
-    public int calculateArea(){
+    public int calculateArea() {
         return length * width;
     }
 
-    public int calculatePerimeter(){
-        return 2*(length + width);
+    public int calculatePerimeter() {
+        return 2 * (length + width);
     }
 
-    public double calculateDiagonal(){
-        return Math.sqrt((Math.pow(length,2) + Math.pow(width, 2)));
+    public double calculateDiagonal() {
+        return Math.sqrt((Math.pow(length, 2) + Math.pow(width, 2)));
     }
 
 
