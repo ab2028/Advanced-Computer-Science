683f82b17522378d7bf351f014fadc0e9d6fa232
ab2028 <avberenji2024@gmail.com>
2025-09-17 14:43:01 -0700

    student profiles lab
    completed
    methods, test students

--- Assignments/Unit 0 Student Profiles Lab/Student.java
+++ Assignments/Unit 0 Student Profiles Lab/Student.java
@@ -1,3 +1,65 @@
-public class Student{
-    
-}
+public class Student {
+    private String name;
+    private String id;
+    private int grade;
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+        this.id = this.generateId();
+    }
+
+    public Student(String name){
+        this.name = name;
+        this.grade = 10;
+        this.id = this.generateId();
+    }
+
+    public String getID(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getGrade(){
+        return this.grade;
+    }
+
+    public void setID(){
+        this.id = this.generateId();
+    }
+
+    public void setName(String input){
+        this.name = input;
+    }
+
+    public void setGrade(int input){
+        this.grade = input;
+    }
+
+
+    public String toString() {
+        return this.name + " is a " + this.grade + "th grade student. Their id is " + this.id; // when is this redundant?
+    }
+
+    public boolean equals(Student other){
+        return this.name == other.name && this.id == other.id && this.grade == other.grade;
+    }
+
+    public String generateId() {
+        String d1 = "" + (int) (Math.random() * 8 + 1);
+        String d2 = "" + (int) (Math.random() * 8 + 1);
+        String d3 = "" + (int) (Math.random() * 8 + 1);
+        String d4 = "" + (int) (Math.random() * 10);
+        String d5 = "" + (int) (Math.random() * 10);
+        String d6 = "" + (int) (Math.random() * 10);
+        String d7 = "" + (int) (Math.random() * 10);
+
+        return d1 + d2 + d3 + d4 + d5 + d6 + d7;
+
+    }
+
+
+    }
--- Assignments/Unit 0 Student Profiles Lab/StudentTester.java
+++ Assignments/Unit 0 Student Profiles Lab/StudentTester.java
@@ -1,3 +1,27 @@
 public class StudentTester {
-    
+    public static void main(String[] args) {
+        Student badStudent = new Student("Badstu Dent");
+        Student goodStudent = new Student("Goodstu Dent");
+        Student jock = new Student("Chad", 12);
+        Student cheerleader = new Student("Brittany", 11);
+
+        System.out.println(badStudent.toString());
+        System.out.println(goodStudent.toString());
+        System.out.println(jock.toString());
+        System.out.println(cheerleader.toString());
+
+        badStudent.setGrade(9);
+        goodStudent.setGrade(12);
+        jock.setGrade(8);
+        cheerleader.setGrade(12);
+
+        System.out.println(badStudent.toString());
+        System.out.println(goodStudent.toString());
+        System.out.println(jock.toString());
+        System.out.println(cheerleader.toString());
+
+        System.out.println("Is Badstu Dent equal to Goodstu Dent? " + badStudent.equals(goodStudent));
+    }
+
+
 }
