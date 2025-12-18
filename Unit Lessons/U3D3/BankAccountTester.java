7347f3fab862a90cb403b74c046ba18cf520ed8d
ab2028 <avberenji2024@gmail.com>
2025-12-18 06:40:46 -0800

    u3d3
    debugging and throwing

---
+++ Unit Lessons/U3D3/BankAccountTester.java
@@ -0,0 +1,17 @@
+public class BankAccountTester {
+    public static void main(String[] args) {
+        BankAccount bank = new BankAccount();
+        try {
+            bank.withdraw(1000);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        try{
+        bank.withdraw(-10);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+}
